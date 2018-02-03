a1484170c4246d17e7c0630738101abaa4298fe9
Kapil Chadha <kapil.chadha@avanade.com>
2018-02-03 21:58:23 +0000

    Scale-1

---
+++ ScaleSet/ScaleVMManually.ps1
@@ -0,0 +1,9 @@
+﻿Login-AzureRmAccount
+
+$ResourceGroupName = "CHAP1"
+$VMName = "SRV1"
+<#$NEWVMSize = "Standard_DS11_v2"#>
+$NEWVMSize = "Standard_DS1_v2"
+$vm = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName
+$vm.HardwareProfile.VmSize = $NEWVMSize
+Update-AzureRmVM -ResourceGroupName $ResourceGroupName -VM $vm
